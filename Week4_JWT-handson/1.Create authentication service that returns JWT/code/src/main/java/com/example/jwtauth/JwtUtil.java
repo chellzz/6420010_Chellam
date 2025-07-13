1a8e117ff0bdfb8f832b400221434cb241e715af
Chellam N <156900588+chellzz@users.noreply.github.com>
2025-07-13 22:57:27 +0530

    Add files via upload

---
+++ Week4_JWT-handson/1.Create authentication service that returns JWT/code/src/main/java/com/example/jwtauth/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.example.jwtauth;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import java.util.Date;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private static final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key)
+                .compact();
+    }
+}
